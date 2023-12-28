e75c38b033c73bb76bba7cda2398d03772716f96
S SHAH <msaqibshah001@gmail.com>
2023-12-28 17:59:17 +0530

    minor fixes and updated readme

--- convert-data.py
+++ convert-data.py
@@ -2,24 +2,25 @@ import json
 import os
 import datetime
 
-def convert_data():
+def convert_data(folder_path,  lang="en"):
     # Specify the file path
-
-    # Specify the folder path
-    folder_path = "data/nptel-pure-set/nptel-pure"
 
     # get absolute path
     folder_path = os.path.abspath(folder_path)
     
     metadata_path = os.path.join(folder_path, "metadata")
     wav_audio_path = os.path.join(folder_path, "wav")
+
+
     new_wav_path = os.path.join(folder_path, "new_wav")
-    new_wav_path = os.path.join(folder_path, "en")
+    if not os.path.exists(new_wav_path):
+            os.mkdir(new_wav_path)
+        
+    new_wav_path = os.path.join(new_wav_path, lang)
 
     if not os.path.exists(new_wav_path):
         os.mkdir(new_wav_path)
 
-    print(wav_audio_path)
     # Iterate over files in the folder
     for file_name in os.listdir(metadata_path):
         json_file_path = os.path.join(metadata_path, file_name)
@@ -51,7 +52,6 @@ def get_new_file_name(json_file_path):
 
         file_name = os.path.basename(json_file_path)
         data = json.load(json_file)
-
         ts_start = data["ts_start"]
         try:
             ts_start = datetime.datetime.strptime(ts_start, "%H:%M:%S.%f").time()
@@ -72,7 +72,6 @@ def get_new_file_name(json_file_path):
 def rename_and_copy_file(old_file_path, new_file_name):
     if os.path.isfile(old_file_path):
         new_file_path = os.path.join(os.path.dirname(old_file_path), new_file_name)
-
     if not os.path.exists(new_file_path):
     #   Copy the content of the file
         with open(old_file_path, 'rb') as f:
@@ -84,4 +83,11 @@ def rename_and_copy_file(old_file_path, new_file_name):
 if __name__ == "__main__":
     # file_name =get_new_file_name("data/nptel-pure-set/nptel-pure/metadata/000ad6c22f20b266297a26479bfde266acaef84e3accec59b5ac9330.json")
     # print(file_name)
-    convert_data()
+
+    # get args from command line
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_folder_path", help="path to the folder containing the data", required=True)
+    parser.add_argument("--lang", help="name of the language for the folder", default="en")
+    args = parser.parse_args()
+    convert_data(folder_path=args.input_folder_path,  lang=args.lang)
--- readme.md
+++ readme.md
@@ -1,7 +1,31 @@
-1. Run convert-data
-2. Run create shards
-3. Run train
+# How To Run
 
-Resources
+This script aims to finetune the [`speechbrain/lang-id-voxlingua107-ecapa`](https://huggingface.co/speechbrain/lang-id-voxlingua107-ecapa) model for improving the language identification of Indian English. The dataset used can be found below.
+
+## 1. Run convert-data
+
+This script is used to convert the [nptel-data](https://github.com/AI4Bharat/NPTEL2020-Indian-English-Speech-Dataset?tab=readme-ov-file#downloads) into the format of [voxlingua](https://bark.phon.ioc.ee/voxlingua107/). This is done because the `create_shards.py` needs to read this data in this format.
+
+`python convert-data.py --input_folder_path path/to/nptel-data`
+
+## 2. Run create shards
+
+This script is used to create WebDataset Shards from the nptel-data (converted). This is done to maxinize the tranning performance of the model.
+
+`python create_shards.py /path/to/data/input /data/finalshards/train`
+
+`python create_shards.py /path/to/data/input /data/finalshards/dev`
+
+Note: The output path should be always set to `/data/finalshards/train` and `/data/finalshards/dev`. The `/train` directoty holds the data for traning and `/dev` directory holds the data for validation.
+
+## 3. Run train
+
+After the data shards are created we can run the `train.py` to start retraning the model.
+
+`python train.py`
+
+## Resources
+
 [Model Recipie](https://github.com/speechbrain/speechbrain/tree/develop/recipes/VoxLingua107)
+
 [Pretraining Example](https://colab.research.google.com/drive/1LN7R3U3xneDgDRK2gC5MzGkLysCWxuC3?usp=sharing#scrollTo=R3B4Dv1Wjfv6)
